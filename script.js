675a875884d8a3a1654dddce5bd20f3db0b3fc90
vedanttiwari07 <tiwarivedant07@gmail.com>
2024-07-07 19:10:50 +0530

    Added project files

---
+++ script.js
@@ -0,0 +1,40 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+function addTask() {
+    if (inputBox.value === "") {
+        alert("Task value cannot be empty!");
+    }
+    else if (inputBox) {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value; 
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    else if (inputBox.value === li.innerHTML) {
+        alert("Entered task already exists!")
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+})
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
